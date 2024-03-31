8530b3faf56b71e3159c2b8d206b748d31acc5dd
ahmedsalehalzabr <ahmedalzabr2@gmail.com>
2024-03-31 04:38:19 -1200

    2

--- GameZone/Controllers/GamesController.cs
+++ GameZone/Controllers/GamesController.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GameZone.Data;
+using GameZone.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace GameZone.Controllers
 {
     public class GamesController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public GamesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -13,7 +23,18 @@ namespace GameZone.Controllers
 
         public IActionResult Create() 
         {
-            return View();
+            CreateGameFormVM viewModel = new()
+            {
+                Categories = _context.Categories
+                .Select(c => new SelectListItem { Value = c.Id.ToString(), Text = c.Name })
+                .OrderBy(c => c.Text)
+                .ToList(),
+                Devices = _context.Devices
+                .Select(a => new SelectListItem { Value = a.Id.ToString(), Text = a.Name })
+                .OrderBy(a => a.Text)
+                .ToList()
+            };
+            return View(viewModel);
         }
     }
 }
--- GameZone/ViewModels/CreateGameFormVM.cs
+++ GameZone/ViewModels/CreateGameFormVM.cs
@@ -1,6 +1,26 @@
-﻿namespace GameZone.ViewModels
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace GameZone.ViewModels
 {
     public class CreateGameFormVM
     {
+        [MaxLength(250)]
+        public string Name { get; set; } = string.Empty;
+
+        [Display(Name = "Category")]
+        public int CategoryId { get; set; }
+
+        public IEnumerable<SelectListItem> Categories { get; set; } = Enumerable.Empty<SelectListItem>();
+
+        public IEnumerable<SelectListItem> Devices { get; set; } = Enumerable.Empty<SelectListItem>();
+
+        [Display(Name = "Supported Devices")]
+        public List<int> SelectedDevices { get; set; } = new List<int>();
+
+        public IFormFile Cover { get; set; } = default!;
+
+        [MaxLength(250)]
+        public string Description { get; set; } = string.Empty;
     }
 }
